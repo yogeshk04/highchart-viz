2e85b0222aac05351aea1735efae3e7dad9c7dab
Yogesh Nikam <36443045+yogeshk04@users.noreply.github.com>
2022-01-05 16:32:52 +0530

    Pie chart visualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
+// import { Chart } from 'highcharts';
+import { donutChart } from './helpers/donutChart';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'highchart-viz';
+  donutChart = new Chart(donutChart);
 }
